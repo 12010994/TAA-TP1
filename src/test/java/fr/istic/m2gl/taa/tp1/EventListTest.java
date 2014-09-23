bb51a0b4406f2f7993f1b4c9dd17e0bca70898be
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-09-23 11:59:14 +0200

    Add new junit test class on EventList

---
+++ src/test/java/fr/istic/m2gl/taa/tp1/EventListTest.java
@@ -0,0 +1,85 @@
+package fr.istic.m2gl.taa.tp1;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventListTest {
+
+	private EntityManager manager;
+	private EventList eventList;
+	private EntityTransaction tx;
+
+	@Before
+	public void init(){
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
+		manager = factory.createEntityManager();
+
+		tx = manager.getTransaction();
+		tx.begin();
+
+		eventList = new EventList(manager);
+	}
+
+	@After
+	public void close(){
+		tx.commit();
+	}
+
+	@Test(expected = NoResultException.class)
+	public void testNoResultExceptionIsThrownWhenEmpty() {
+		Event event = (Event) manager.createQuery(
+				"FROM Event WHERE id=1")
+				.getSingleResult();
+	}
+
+	@Test(expected = Exception.class)
+	public void ExceptionIsThrownWhenMalformedEvent() {
+		eventList.addEvent("", "");
+	}
+
+	@Test
+	public void testAddOneEvent() {
+
+		eventList.addEvent("today", "Rennes");
+		Event event = (Event) manager.createQuery(
+				"FROM Event WHERE id=1")
+				.getSingleResult();
+		assertEquals(1, event.getId());
+		assertEquals("today", event.getDate());
+		assertEquals("Rennes", event.getPlace() );
+	}
+
+	@Test
+	public void testAddThreeEvent() {
+
+		eventList.addEvent("11-11-11 11h11", "Rennes");
+		eventList.addEvent("12-12-12 12h12", "Paris");
+		eventList.addEvent("13-12-13 13h13", "Berlin");
+
+		Event event1 = (Event) manager.createQuery(
+				"FROM Event WHERE id=1")
+				.getSingleResult();
+		Event event2 = (Event) manager.createQuery(
+				"FROM Event WHERE id=2")
+				.getSingleResult();
+		Event event3 = (Event) manager.createQuery(
+				"FROM Event WHERE id=3")
+				.getSingleResult();
+		assertEquals(1, event1.getId());
+		assertEquals("11-11-11 11h11", event1.getDate());
+		assertEquals("Rennes", event1.getPlace() );
+		assertEquals(2, event2.getId());
+		assertEquals("12-12-12 12h12", event2.getDate());
+		assertEquals("Paris", event2.getPlace() );
+		assertEquals(3, event3.getId());
+		assertEquals("13-12-13 13h13", event3.getDate());
+		assertEquals("Berlin", event3.getPlace() );
+	}
+
+}
